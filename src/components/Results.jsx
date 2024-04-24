046110e3a1dc02af1dcb4a53dcc506d46a2beeb8
raghadSabri6 <raghadsabri1111@gmail.com>
2024-04-24 17:13:27 +0300

    Add investment calculator components and logic

---
+++ src/components/Results.jsx
@@ -0,0 +1,39 @@
+import { calculateInvestmentResults, formatter } from "../util/investment.js";
+export default function Results({ input }) {
+  const results = calculateInvestmentResults(input);
+  const initialInvestment =
+    results[0].valueEndOfYear -
+    results[0].interest -
+    results[0].annualInvestment;
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Investment Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {results.map((yearData) => {
+          const totalIntreset =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
+          const totalAmountInvestment = yearData.valueEndOfYear - totalIntreset;
+          return (
+            <tr id={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalIntreset)}</td>
+              <td>{formatter.format(totalAmountInvestment)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
